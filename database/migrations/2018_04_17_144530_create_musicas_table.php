a8d00b19897095c5e2c3b47abb0c345f6275d97c
Bruno Landeiro <bruno.landeiro@quality.com.br>
2018-04-17 14:23:59 -0300

    inicio

---
+++ database/migrations/2018_04_17_144530_create_musicas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusicasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('musicas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('cantor');
+            $table->string('link');
+            $table->text('letra');
+            $table->string('youtube');
+            $table->string('img');
+            $table->timestamps('create_at');
+            $table->timestamps('update_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musicas');
+    }
+}
